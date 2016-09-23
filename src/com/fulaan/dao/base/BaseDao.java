06b6eafe2d2605a0727739a8e2e8fe5d8a666154
felix.xu <felix.xu@fulaan.com>
2016-09-23 10:12:36 +0800

    公共数据持久类

---
+++ src/com/fulaan/dao/base/BaseDao.java
@@ -0,0 +1,115 @@
+package com.fulaan.dao.base;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import org.hibernate.Session;
+
+public interface BaseDao {
+	
+	/**
+	 * 获取session
+	 * @return
+	 */
+	public Session getSession();
+	
+	/**
+	 * 获取session
+	 * @param isAllow 是否重新创建
+	 * @return
+	 */
+	public Session getSession(boolean isAllow);
+	
+	/**
+	 * 保存一个对象
+	 * @param obj
+	 */
+	public void save(Object obj);
+	
+	/**
+	 * 保存或更新对象
+	 * @param obj
+	 */
+	public void saveOrUpdate(Object obj);
+	
+	/**
+	 * 批量保存或更新集合中对象
+	 * @param collection
+	 */
+	public void saveOrUpdateAll(Collection<?> collection);
+	
+	/**
+	 * 更新对象
+	 * @param obj
+	 */
+	public void update(Object obj);
+	
+	/**
+	 * 更新保存对象
+	 * @param obj
+	 */
+	public void merge(Object obj);
+	
+	/**
+	 * 根据实体对象和主键ID查找
+	 * @param entity
+	 * @param id 主键id
+	 * @return
+	 */
+	public <T> T get(Class<T> entity, Serializable id);
+	
+	/**
+	 * 获取实体所对应的表中所有数据
+	 * @param entity
+	 * @return
+	 */
+	public <T> List<T> findAll(Class<T> entity);
+	
+	/**
+	 * 根据查询语句进行查询
+	 * @param query 查询语句
+	 * @return
+	 */
+	public List find(String query);
+	
+	/**
+	 * 根据查询语句进行查询
+	 * @param query 查询语句
+	 * @param param 参数
+	 * @return
+	 */
+	public List find(String query, Object param);
+	
+	/**
+	 * 根据查询语句进行查询
+	 * @param query 查询语句
+	 * @param objParams 参数数组
+	 * @return
+	 */
+	public List find(String query, Object[] objParams);
+	
+	/**
+	 * 删除
+	 * @param obj
+	 */
+	public void delete(Object obj);
+	
+	/**
+	 * 查询实体所对应的表中记录条数
+	 * @param entity
+	 * @return
+	 */
+	public long countAll(Class<?> entity);
+	
+	/**
+	 * 根据查询语句计算条数
+	 * @param queryString
+	 * @return
+	 */
+	public long count(String queryString);
+	
+	public long count(String queryString, Object param);
+	
+	public long count(String queryString, Object[] params);
+}
---
+++ src/com/fulaan/dao/base/BaseDaoImpl.java
@@ -0,0 +1,152 @@
+package com.fulaan.dao.base;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate3.SessionFactoryUtils;
+import org.springframework.stereotype.Repository;
+
+@Repository("baseDao")
+public class BaseDaoImpl implements BaseDao{
+	
+	private JdbcTemplate jdbcTemplate;
+	
+	private HibernateTemplate hibernateTemplate;
+	
+	protected SessionFactory sessionFactory;
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	@Resource
+	public void setJdbcTemplate(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+
+	@Resource
+	public void setHibernateTemplate(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+		this.hibernateTemplate = new HibernateTemplate(sessionFactory);
+		this.hibernateTemplate.setCacheQueries(true);
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	@Override
+	public void save(Object obj) {
+		getHibernateTemplate().save(obj);
+	}
+
+	@Override
+	public void saveOrUpdate(Object obj) {
+		getHibernateTemplate().saveOrUpdate(obj);
+	}
+
+	@Override
+	public void saveOrUpdateAll(Collection<?> collection) {
+		getHibernateTemplate().saveOrUpdateAll(collection);
+	}
+
+	@Override
+	public void update(Object obj) {
+		getHibernateTemplate().update(obj);
+	}
+
+	@Override
+	public void merge(Object obj) {
+		getHibernateTemplate().merge(obj);
+	}
+
+	@Override
+	public <T> T get(Class<T> entity, Serializable id) {
+		return (T)getHibernateTemplate().get(entity, id);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public <T> List<T> findAll(Class<T> entity) {
+		return getHibernateTemplate().find("from " + entity.getName());
+	}
+
+	@Override
+	@SuppressWarnings("rawtypes")
+	public List find(String query) {
+		return getHibernateTemplate().find(query);
+	}
+
+	@Override
+	@SuppressWarnings("rawtypes")
+	public List find(String query, Object param) {
+		return getHibernateTemplate().find(query, param);
+	}
+
+	@Override
+	@SuppressWarnings("rawtypes")
+	public List find(String query, Object[] objParams) {
+		return getHibernateTemplate().find(query, objParams);
+	}
+
+	@Override
+	public void delete(Object obj) {
+		getHibernateTemplate().delete(obj);
+	}
+
+	@Override
+	public Session getSession() {
+		return getSession(hibernateTemplate.isAllowCreate());
+	}
+
+	@Override
+	public Session getSession(boolean isAllow) {
+		return isAllow ? 
+				SessionFactoryUtils.
+				getSession(hibernateTemplate.getSessionFactory(), 
+						hibernateTemplate.getEntityInterceptor(), 
+						hibernateTemplate.getJdbcExceptionTranslator()) 
+				: SessionFactoryUtils.getSession(hibernateTemplate.getSessionFactory(), false);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public long countAll(Class<?> entity) {
+		List<Long> list = getHibernateTemplate().find("select count(*) from" + entity.getName());
+		return (list != null && list.size() > 0) ? list.get(0) : 0L;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public long count(String queryString) {
+		List<Long> list = find(queryString);
+		return (list != null && list.size() > 0) ? list.get(0) : 0L;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public long count(String queryString, Object param) {
+		List<Long> list = find(queryString, param);
+		return (list != null && list.size() > 0) ? list.get(0) : 0L;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public long count(String queryString, Object[] params) {
+		List<Long> list = find(queryString, params);
+		return (list != null && list.size() > 0) ? list.get(0) : 0L;
+	}
+	
+}

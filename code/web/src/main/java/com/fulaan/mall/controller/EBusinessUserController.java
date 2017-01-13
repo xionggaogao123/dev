589f4b183740eed8f0a5a514f5945440f132f60d
scott.zhangli <2678089639@qq.com>
2017-01-13 14:41:43 +0800

    消息发送

--- code/web/src/main/java/com/fulaan/mall/controller/EBusinessUserController.java
+++ code/web/src/main/java/com/fulaan/mall/controller/EBusinessUserController.java
@@ -11,6 +11,7 @@ import com.fulaan.mall.service.EBusinessVoucherService;
 import com.fulaan.pojo.Validate;
 import com.fulaan.user.controller.UserController;
 import com.fulaan.user.service.UserService;
+import com.fulaan.utils.KeyWordFilterUtil;
 import com.pojo.app.SessionValue;
 import com.pojo.ebusiness.EVoucherEntry;
 import com.pojo.forum.FInvitationEntry;
@@ -85,22 +86,40 @@ public class EBusinessUserController extends BaseController {
     public Map<String, Object> shortMessage(HttpServletRequest request, HttpServletResponse response) {
 
         Map<String, Object> model = new HashMap<String, Object>();
-        String mobile="18817366135";
+        List<String> mobiles= KeyWordFilterUtil.message_list;
         String msg = "【复兰教育社区】冬季大赛来袭！火热“春节才艺秀”、“家乡的冬天”摄影比赛，缤纷大奖，邀你参加！登录“复兰教育社区”APP，进入【发现】中的【大赛】版块或访问www.fulaan.com的【大赛】版块参与比赛。";
         try {
-            String resp = batchSend(url, account, pswd, mobile, msg, needstatus, product, extno);
-            String responseCode = resp.split("\\n")[0].split(",")[1];
-            if (responseCode.equals("0")) {
-                model.put("code", 200);
-                model.put("message", resp);
+            List<String> send=new ArrayList<String>();
+            for(String mobile:mobiles) {
+                send.add(mobile);
+                if(send.size()==2) {
+                    sendMessageInfo(send,msg);
+                    send.clear();
+                }
             }
-
+            if(send.size()>0){
+                sendMessageInfo(send,msg);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return model;
     }
 
+    private void sendMessageInfo(List<String> send,String msg)throws Exception{
+        StringBuffer buffer=new StringBuffer();
+        for(String item:send){
+            buffer.append(item+",");
+        }
+        String mobile=buffer.toString().substring(0,buffer.toString().length()-1);
+        String resp = batchSend(url, account, pswd, mobile, msg, needstatus, product, extno);
+        String responseCode = resp.split("\\n")[0].split(",")[1];
+        if (responseCode.equals("0")) {
+            Thread.sleep(2000);
+        } else {
+            Thread.sleep(2000);
+        }
+    }
 
     /**
      * 获取验证码

0fd88d6cd2c9470e3993823bbec00cd48ffa662b
scott.zhangli <2678089639@qq.com>
2017-10-27 14:03:11 +0800

    计算

--- code/web/src/main/java/com/fulaan/appmarket/service/AppMarketService.java
+++ code/web/src/main/java/com/fulaan/appmarket/service/AppMarketService.java
@@ -16,6 +16,8 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 
 /**
@@ -100,9 +102,9 @@ public class AppMarketService {
             totalStar+=dto.getStar()*dto.getCount();
         }
         if(total!=0){
-            avgStar=(double)totalStar/(double)total;
+            avgStar=divide((double)totalStar,(double)total,1);
             for(AppDetailStarStatisticDTO dto:initDatas){
-                double p=(double)dto.getCount()/(double)total;
+                double p=divide(mul((double)dto.getCount(),100D),(double)total,1);
                 dto.setPercent(p);
             }
         }
@@ -119,6 +121,21 @@ public class AppMarketService {
         retMap.put("page",page);
         retMap.put("pageSize",pageSize);
         return retMap;
+    }
+
+    public static Double mul(Double value1, Double value2) {
+        BigDecimal b1 = new BigDecimal(Double.toString(value1));
+        BigDecimal b2 = new BigDecimal(Double.toString(value2));
+        return b1.multiply(b2).doubleValue();
+    }
+
+    public static Double divide(Double dividend, Double divisor, Integer scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(Double.toString(dividend));
+        BigDecimal b2 = new BigDecimal(Double.toString(divisor));
+        return b1.divide(b2, scale, RoundingMode.HALF_UP).doubleValue();
     }
 
     public List<AppDetailStarStatisticDTO> generateInitData(){

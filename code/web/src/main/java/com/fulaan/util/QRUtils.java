9487ce2f3feedff20940368fd2781bfa988a09d8
scott.zhangli <2678089639@qq.com>
2017-11-06 15:03:57 +0800

    二维码前缀路径

--- code/web/src/main/java/com/fulaan/util/QRUtils.java
+++ code/web/src/main/java/com/fulaan/util/QRUtils.java
@@ -13,9 +13,10 @@ import java.io.FileOutputStream;
  */
 public class QRUtils {
 
-    //private static final String prev_url="http://www.fulaan.com";
+//    private static final String prev_url="http://www.fulaan.com";
+    private static final String prev_url="http://101.37.190.133";
    // private static final String prev_url="http://121.40.229.225:8080";
-  private static final String prev_url="http://192.168.1.35:8087";
+//  private static final String prev_url="http://192.168.1.35:8087";
 
     /**
      * 生成社区二维码路径

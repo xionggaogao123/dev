77866b0635f19760ea7c0d8b54afd855faa33255
James ZQ <zhangqingandcc@163.com>
2017-12-19 17:57:54 +0800

    mqtt扫描二维码登陆

--- code/web/src/main/java/com/fulaan/service/CommunityService.java
+++ code/web/src/main/java/com/fulaan/service/CommunityService.java
@@ -124,7 +124,7 @@ public class CommunityService {
         dto.setName("");
         dto.setFileUrl("");
         dto.setSourceId(communityId.toString());
-        dto.setContent(entry.getSearchId());
+        dto.setContent(seqId);
         dto.setFileType(1);
         dto.setSourceName(entry.getCommunityName());
         dto.setSourceType(1);

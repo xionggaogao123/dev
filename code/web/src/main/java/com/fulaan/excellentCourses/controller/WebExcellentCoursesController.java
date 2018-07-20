8726ae59d8f6f58cd55af861bf1006815f0ecf93
james <james.zhang@fulaan.com>
2018-07-20 22:42:39 +0800

    学生上课

--- code/web/src/main/java/com/fulaan/excellentCourses/controller/WebExcellentCoursesController.java
+++ code/web/src/main/java/com/fulaan/excellentCourses/controller/WebExcellentCoursesController.java
@@ -105,6 +105,31 @@ public class WebExcellentCoursesController extends BaseController {
     }
 
     /**
+     *  去上课
+     */
+    @SessionNeedless
+    @ApiOperation(value = "去上课", httpMethod = "GET", produces = "application/json")
+    @ApiResponses( value = {@ApiResponse(code = 200, message = "Successful — 请求已完成",response = String.class),
+            @ApiResponse(code = 400, message = "请求中有语法问题，或不能满足请求"),
+            @ApiResponse(code = 500, message = "服务器不能完成请求")})
+    @RequestMapping("/gotoWebClass")
+    @ResponseBody
+    public String gotoWebClass(@ApiParam(name = "id", required = false, value = "id") @RequestParam(value="id",defaultValue = "") String id,
+                               @ApiParam(name = "userId", required = false, value = "userId") @RequestParam(value="userId",defaultValue = "") String userId){
+        RespObj respObj=new RespObj(Constant.FAILD_CODE);
+        try {
+            respObj.setCode(Constant.SUCCESS_CODE);
+            Map<String,Object>  dto = excellentCoursesService.gotoClass(new ObjectId(id),new ObjectId(userId));
+            respObj.setMessage(dto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            respObj.setCode(Constant.FAILD_CODE);
+            respObj.setErrorMessage(e.getMessage());
+        }
+        return JSON.toJSONString(respObj);
+    }
+
+    /**
      * 我所有报名的课程（分页）
      */
     @ApiOperation(value = "我所有报名的课程", httpMethod = "POST", produces = "application/json")

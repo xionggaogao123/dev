b1f2c3268d492ed7852125a3fc993415aedc4724
taotao.chan <arron.chan@fulaan.com>
2018-11-07 14:00:55 +0800

    128

--- code/web/src/main/java/com/fulaan/backstage/service/BackStageAdminManageService.java
+++ code/web/src/main/java/com/fulaan/backstage/service/BackStageAdminManageService.java
@@ -48,7 +48,12 @@ public class BackStageAdminManageService {
         String msg = "";
         UserEntry userEntry = userDao.getGenerateCodeEntry(map.get("userJiaId").toString());
         if (null == userEntry){
-            msg = "用户不存在，请检查用户Id";
+            msg = "用户不存在，请检查用户ID！";
+            return msg;
+        }
+        List<UserLogResultEntry> resultEntries = userLogResultDao.getLogsByUserId(userEntry.getID());
+        if (resultEntries.size() > 0){
+            msg = "此用户已经是管理员！";
             return msg;
         }
         if ("".equals(map.get("id")) || null == map.get("id")) {//新增管理员权限
